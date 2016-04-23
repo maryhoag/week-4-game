f144a953d7760261bc96519ff6e02385b3f4e919
Mary Hoag <marymcevoyhoag@gmail.com>
2016-04-23 18:39:47 -0400

    added html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,12 +2,14 @@
 	//health, attack( which += with each attack), counter
 	//also name, photo, maybe sound effect?
 	//while enemy, only counter and unchangeable
-var rey = {name: "Rey", image:"src='.//css/images.rey.jpg' alt='Rey' ", health: 20, attack: 5, counter: 6};
-var kylo = {name: "Kylo", image:"src='.//css/images.kylo.jpg' alt='Kylo' ", health: 22, attack:4, counter:5};
-var finn = {name:"Finn", image:"src='.//css/images/finn.jpg' alt='Finn' ", health:25, attack: 3, counter:7};
-var phasma = {name: "Phasma", image:"src='.//css.images/phasma.jpg' alt='Phasma'", health:20, attack:6, counter:5};
+var rey = {name: "Rey", image:"src='.//css/images.rey.jpg' alt='Rey' height='200px' width='200px;' ", health: 20, attack: 5, counter: 6};
+var kylo = {name: "Kylo", image:"src='.//css/images.kylo.jpg' alt='Kylo' height='200px' width='200px' ", health: 22, attack:4, counter:5};
+var finn = {name:"Finn", image:"src='.//css/images/finn.jpg' alt='Finn' height='200px' width='200px'", health:25, attack: 3, counter:7};
+var phasma = {name: "Phasma", image:"src='.//css.images/phasma.jpg' alt='Phasma' height='200px' width=200px' ", health:20, attack:6, counter:5};
 
 //collect user pick for combatant
+$
+
 
 //remove other players to enemy corral
 
